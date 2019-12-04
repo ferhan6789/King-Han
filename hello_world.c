ea8600a0a30c441aa562ccfc6db2d244b2058c58
Jonathan Michael Forbes <jforbes>
2019-12-04 11:21:20 -0600

    bcd1 and bcd2 display numbers from 1-9

--- hello_world.c
+++ hello_world.c
@@ -16,24 +16,22 @@ volatile int * HEX3_HEX0_ptr	= (int *) HEX3_HEX0_BASE;	// HEX3_HEX0 address
 
 
 void onButtonPress(void *context, alt_u32 id);
-void onSwitch(void *context, alt_u32 id);
+void onSwitch();
 
 int main(){
 	*(HEX3_HEX0_ptr) = 0x3f3f;
-
-	int r2 = alt_irq_register(-1, NULL, onSwitch);
 	int r1 = alt_irq_register(1, NULL, onButtonPress);	//0 if successful
 
-	printf("Hello from Nios II. got %d and %d!\n", r1, r2);
+	printf("Hello from Nios II. got %d and %d!\n", r1);
 
 	// Addresses to enable interrupts for
     *(btn_ptr + 2) = 0xe;	// buttons 1-3
-    *(switch_ptr + 2) = 0x0;
-    *(switch_ptr) = 0x0;
 
 
-
-	while(1){}
+	while(1){
+		onSwitch();
+		usleep(150000);
+	}
 	return 0;
 }
 
@@ -47,9 +45,47 @@ void onButtonPress(void *context, alt_u32 id){
 
 }
 
-void onSwitch(void *context, alt_u32 id){
-	printf("Hal9000\n");
+
+
+/*
+ * Resets a range of bits between rangeMin and rangeMax
+ * to 0
+ */
+int ignoreBits(int rangeMin, int rangeMax, int number){
+	int i = 0;
+	for(i = 0; i <=9; ++i){
+		if(i >= rangeMin && i <= rangeMax) number &= ~(1UL << i);
+	}
+	return number;
 }
+
+
+
+
+
+/*
+ * handles switch polling operations
+ */
+void onSwitch(){
+	//conditions for initialization & preparation
+	int switchValue = *(switch_ptr);
+	int switchNine = *(switch_ptr);
+	switchNine = ignoreBits(0,8,switchNine);
+
+	printf("sw9: %d", switchNine);
+	if(switchNine == 512){
+		//set the parking rate (initialization)
+		int bcd1 = ignoreBits(4,9,switchValue);
+		int bcd2 = ignoreBits(0,3,switchValue);
+		bcd2 = ignoreBits(8,9,bcd2);
+		bcd2 >>= 4;
+		printf("\nbcd1: %d, bcd2: %d\n", bcd1, bcd2);
+	}else if(switchNine == 0){
+
+	}
+	printf("sw1: %d\n", switchValue);
+}
+
 
 
 /* The function below takes two arguments: value which is the value in decimal
